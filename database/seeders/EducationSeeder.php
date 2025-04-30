55fba5cee7e6474a0f54ba6009242de0acf0c806
shubham kumar <suraj2002fake@gmail.com>
2025-04-30 08:14:25 +0530

    changes on seeder

--- database/seeders/EducationSeeder.php
+++ database/seeders/EducationSeeder.php
@@ -15,15 +15,27 @@ class EducationSeeder extends Seeder
      */
     public function run(): void
     {
+        Education::truncate();
         Education::create([
-            'degree' => 'Bachelor of Science',
-            'institution' => 'XYZ University',
-            'field_of_study' => 'Computer Science',
-            'start_date' => '2018-08-01',
-            'end_date' => '2022-06-30',
+            'degree' => 'Polytechnic Diploma',
+            'institution' => 'Government Polytechnic Kanpur',
+            'field_of_study' => 'Information Technology',
+            'start_date' => '2021-07-15',
+            'end_date' => '2024-06-30',
             'currently_studying' => false,
-            'grade' => 3.8,
-            'description' => 'Completed with honors',
+            'grade' => 8.1,
+            'description' => 'Completed Polytechnic Diploma program with honors, showcasing strong academic and technical skills.',
+            'user_id' => User::where('role', 'ADMIN')->value('id'),
+        ]);
+        Education::create([
+            'degree' => 'Bachelor of Technology',
+            'institution' => 'Abdul Kalam Technical University',
+            'field_of_study' => 'Computer Science',
+            'start_date' => '2024-07-01',
+            'end_date' => '2027-06-30',
+            'currently_studying' => true,
+            'grade' => 7.5,
+            'description' => 'Currently pursuing this degree and working towards successful completion.',
             'user_id' => User::where('role', 'ADMIN')->value('id'),
         ]);
     }
--- database/seeders/ProfessionalExperienceSeeder.php
+++ database/seeders/ProfessionalExperienceSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\ProfessionalExperience;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProfessionalExperienceSeeder extends Seeder
 {
@@ -14,27 +15,32 @@ class ProfessionalExperienceSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('experience_technology')->truncate();
+        DB::table('professional_experiences')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         ProfessionalExperience::insert([
             [
-                'job_title' => 'Software Engineer',
-                'company' => 'Tech Solutions Inc.',
-                'location' => 'New York, USA',
-                'start_date' => '2020-05-01',
-                'end_date' => null,
+                'job_title' => 'Java Software Engineer',
+                'company' => 'SV Infotech',
+                'location' => 'Kanpur, India',
+                'start_date' => '2023-08-01',
+                'end_date' => "2024-06-01",
                 'currently_working' => true,
-                'description' => 'Developing web applications using Laravel and Vue.js.',
+                'description' => "Designed and maintained scalable, high-performance Java applications for multiple clients, ensuring clean architecture, code quality, and timely delivery.",
                 'user_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'job_title' => 'Backend Developer',
-                'company' => 'Innovative Tech Ltd.',
-                'location' => 'San Francisco, USA',
-                'start_date' => '2018-03-15',
-                'end_date' => '2020-04-30',
+                'company' => 'Smart Web Arts Pvt. Ltd.',
+                'location' => 'Lucknow, India',
+                'start_date' => '2024-06-15',
+                'end_date' => null,
                 'currently_working' => false,
-                'description' => 'Built scalable APIs and optimized database queries.',
+                'description' => 'Worked on the development of a web-based application using PHP and Laravel, focusing on backend services and RESTful APIs.',
                 'user_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
